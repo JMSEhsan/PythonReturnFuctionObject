3f4aba5e01a6633f32b5c92c41d6591178ee8e09
Ehsan Jalali <JMSEhsan@gmail.com>
2022-06-19 00:42:15 -0600

    commit

--- ReturnFunctionObject.py
+++ ReturnFunctionObject.py
@@ -1,5 +1,17 @@
 #Ref.: Bell, Ana, Get Programming Lean to Code with Python, Manning 2018
+#returning a function object from another function
 
 def grumpy():
-    
+    print("I am a grumpy cat: ")
+    def no_n_times(n):
+        print("No", n, "times...")
+        def no_m_more_times(m):
+            print("...and no", m, "more times")
+            for i in range(n+m):
+                print("no")
+        return no_m_more_times
+    return no_n_times
+grumpy()(4)(2)
+
+
 
